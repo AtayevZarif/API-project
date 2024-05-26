5de6fb18a3c0ef93fd3936811d27ecb84e9801a5
AtayevZarif <atayev1234567890@gmail.com>
2024-05-27 00:52:26 +0500

    Succsess

---
+++ main.js
@@ -0,0 +1,28 @@
+let main = document.querySelector("main")
+
+
+fetch("https://fakestoreapi.com/users")
+    .then(res => res.json())
+    .then(users => myFunction(users))
+    .catch(err => console.error("fetch error: ", err))
+
+
+let myFunction = (users) => {
+    users.forEach(user => {
+        let container = document.createElement("div")
+        let name = document.createElement("h1")
+        let username = document.createElement("h2")
+        let address = document.createElement("h2")
+        let email = document.createElement("h2")
+        
+        name.textContent = `Name: ${user.name.firstname}`
+        username.textContent = `Username: ${user.username}`
+        address.textContent = `Address: ${user.address.street}`
+        email.textContent = `Email: ${user.email}`
+
+        container.append(name, username, address, email)
+        main.appendChild(container)
+    });
+}
+
+myFunction()
